e7bbb014d9b8ab4a917c87f3d80796b747c863a5
SeifMohy <seifmohy@gmail.com>
2022-07-10 13:36:57 +0200

    setup

---
+++ src/Components/filteringButtons.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const filteringButtons = () => {
+  return (
+    <div>filteringButtons</div>
+  )
+}
+
+export default filteringButtons
---
+++ src/Components/tabels.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const tabels = () => {
+  return (
+    <div>tabels</div>
+  )
+}
+
+export default tabels
