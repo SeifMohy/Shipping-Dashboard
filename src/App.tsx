8056abeea22aff75c44826127f8a716eb44b5194
SeifMohy <seifmohy@gmail.com>
2022-07-31 20:41:39 +0200

    fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Components/navbar";
 import FilteringButtons from "./Components/filteringButtons";
 import SearchBar from "./Components/searchBar";
 import Tabels from "./Components/tabels";
 import { shipments } from "./Utils";
+import axios from "axios";
 
 function App() {
   const [displayedShipments, setDisplayedShipments] = useState(shipments);
   const [updatedShipments, setUpdatedShipments] = useState(shipments);
 
+  async function getAndSetShipments() {
+    const res = await axios.get("https://localhost:7173/api/shipments/");
+    const shipments = res.data;
+    setDisplayedShipments(shipments);
+    setUpdatedShipments(shipments);
+    console.log(shipments);
+  }
+  useEffect(() => {
+    getAndSetShipments();
+  }, []);
   return (
     <div className="bg-gray-100 p-2 h-screen">
       <Navbar />
